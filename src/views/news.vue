2864b94db2eaec7408c6dc7dec78064428ce55db
张丽 <2090885356@qq.com>
2017-05-18 16:58:25 +0800

    UPDATE

--- src/views/news.vue
+++ src/views/news.vue
@@ -197,7 +197,11 @@
     }
     .comment-content h3{
         font-size: 1.2rem;
-        color:#f3f3f4;
+        color:#333;
+        width:50%;
+        white-space:nowrap;/* 不换行 */
+        overflow:hidden;/* 内容超出宽度时隐藏超出部分的内容 */
+        text-overflow:ellipsis;/* 当对象内文本溢出时显示省略标记(...) ；需与overflow:hidden;一起使用。*/
     }
     .comment-content h3 span{
         font-size: 0.9rem;
